1dfce0d67443915ce9ac87bd3a32dd8e5883c80f
mtisby <marieltisby1998@gmail.com>
2021-11-29 17:01:27 -0800

    pkgs

--- server/app.js
+++ server/app.js
@@ -2,8 +2,31 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from 'mongoose';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
+const dbUrl = 'mongodb://localhost:27017/cinema-save';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+const port = process.env.PORT || 3060;
+
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+app.listen(app.listen(port, () => {
+    console.log(`listening on : ${port}`)
+}))
