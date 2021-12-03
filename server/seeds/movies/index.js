1c77c16ec0ead523d462c5da93cbed2b2c9783d4
mtisby <marieltisby1998@gmail.com>
2021-12-02 17:51:28 -0800

    destructuring api data

--- server/seeds/movies/index.js
+++ server/seeds/movies/index.js
@@ -1,23 +1,47 @@
 import mongoose from "mongoose"
 import { Movie } from "../../models/movies.js"
-import { movie_data } from "../../public/apis/imdb-api/getMovies.js"
+// import { movie_data } from "../../public/apis/imdb-api/getMovies.js"
 import fetch from "node-fetch"
 
 import dotenv from "dotenv"
-dotenv.config({ path: ".env" })
+dotenv.config({ path: "../../.env" })
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/cinema-save';
-mongoose.connect(dbUrl);
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/cinema-save';
+// mongoose.connect(dbUrl);
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
 
 const imdb_url = `https://imdb-api.com/en/API/MostPopularMovies/${process.env.IMDB_KEY}`;
-const omdb_url = `http://www.omdbapi.com/?apikey=${process.env.OMDb_KEY}&`;
+const omdb_url1 = `http://www.omdbapi.com/?t=`
+const omdb_url2 = `&apikey=${ process.env.OMDB_KEY }`;
 const watchmode_url = `https://api.watchmode.com/v1/search/?apiKey=${process.env.WATCHMODE_KEY}&search_field=name&search_value=`
+
+
+// functions 
+// function getMovieData() {
+//     let imdbData = fetch(imdb_url);
+//     let omdbData = fetch(omdb_url);
+//     //let watchModeData = fetch(watchmode_url);
+
+    
+
+//     Promise.all([imdbData, omdbData])
+//         .then(values => Promise.all(values.map(value => value.json())))
+//         .then(finalVals => {
+//             let imdbMovies = finalVals[0];
+//             let omdbMovies = finalVals[1];
+            
+
+//             console.log(omdbMovies)
+//         })
+// }
+
+// getMovieData()
+
 
 // fetch data from imdb-api
 fetch(imdb_url)
@@ -27,23 +51,32 @@ fetch(imdb_url)
     .then(imdbData => {
         let N = imdbData['items'].length;
         let imdbMovies = imdbData['items'];
-
-        for (i = 0; i < N; i++) {
+        
+        for (var i = 0; i < 2; i++) {
             let imdbMovie = imdbMovies[i];
 
             // call omdb-api here
             // call watchmode-api here
+            let queriedTitle = imdbMovie.title.split(' ').join('+');
+            fetch(omdb_url1 + queriedTitle + omdb_url2)
+                .then(res => {
+                    return res.json()
+                })
+                .then(omdbData => {
+                    console.log(omdbData)
+                })
 
-            const movieObj = new Movie({
-                title: imdbMovie.fullTitle,
-                year: imdbMovie.year,
-                imdbRating: imdbMovie.imDbRating,
-                // genre: omdbMovie.genre,
-                // stream: watchMode
-                // description: omdbMovie.description,
-                // languages: omdbMovie.languages,
-                poster: imdbMovie.image
-            })
+
+            // const movieObj = new Movie({
+            //     title: imdbMovie.fullTitle,
+            //     year: imdbMovie.year,
+            //     imdbRating: imdbMovie.imDbRating,
+            //     // genre: omdbMovie.genre,
+            //     // stream: watchMode
+            //     // description: omdbMovie.description,
+            //     // languages: omdbMovie.languages,
+            //     poster: imdbMovie.image
+            // })
             
         }
 
