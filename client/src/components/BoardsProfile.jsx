d3c1cf6ab107723c0d88e10861339007d0d348e7
mtisby <marieltisby1998@gmail.com>
2021-12-26 23:28:13 -0800

    styling

--- client/src/components/BoardsProfile.jsx
+++ client/src/components/BoardsProfile.jsx
@@ -6,6 +6,7 @@ import {images} from '../scripts/image.js'
 export const BoardsProfile = (props) => {
     const userid = props.data;
     const [boards, setBoards] = useState([]);
+    const [bkImg, setBkgImg] = useState(Boolean);
 
     const boardStyle = {
         backgroundImage: 'url(' + images[0] + ')',
@@ -24,14 +25,14 @@ export const BoardsProfile = (props) => {
     }, []);
     
     const allBoards = boards.map((i) => {
-        let bkgImg = false;
         if (i.pins[0]){ 
-            bkgImg = true
+            setBkgImg(true)
         }
+
         return (
             <div className='board-contianer-profile'>
                 <Link to={`/profile/${userid}/board/${i._id}`}>
-                    <div style={boardStyle}>
+                    <div style={bkImg ? { backgroundImage: i.pins[0].poster } : {}}>
                         <h3>{ i.title }</h3>
                     </div>
                 </Link>
