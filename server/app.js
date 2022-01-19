280d21d92f7178e737a8e69ba725e8c7def0188d
mtisby <marieltisby1998@gmail.com>
2022-01-19 10:20:17 -0800

    modify server

--- server/app.js
+++ server/app.js
@@ -100,7 +100,7 @@ app.use(session(sessionConfig))
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://modest-shirley-e38b5f.netlify.app/",
     credentials: true
 }))
 
